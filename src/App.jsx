5131eca9da95088e106021062be41c74826d3855
Shawn Yates <yates.shawnp@gmail.com>
2017-05-03 21:25:27 +0000

    connected socket

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,30 @@ class App extends Component {
       }
   }
 
+  // componentDidMount() {
+  //   console.log("componentDidMount <App />");
+  //   setTimeout(() => {
+  //     console.log("Simulating incoming message");
+  //     // Add a new message to the list of messages in the data store
+  //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //     const messages = this.state.messages.concat(newMessage)
+  //     // Update the state of the app component.
+  //     // Calling setState will trigger a call to render() in App and all child components.
+  //     this.setState({messages: messages})
+  //   }, 3000);
+  // }
+
+  onNewMessage(event) {
+    const id = this.state.messages.length + 1;
+    const newMessage = {id: id, username: this.state.currentUser.name, content: event.target.value};
+    const messages = this.state.messages.concat(newMessage);
+    this.setState({messages: messages});
+  }
+
+  onUserChange(event) {
+    this.setState({currentUser: {name: event.target.value}});
+  }
+
 
   render() {
     console.log("Rendering <App/>");
@@ -35,10 +59,22 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messages={this.state.messages} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar currentUser={this.state.currentUser}
+                 onNewMessage={this.onNewMessage.bind(this)}
+                 onUserChange={this.onUserChange.bind(this)} />
       </div>
     );
   }
+
+  componentDidMount() {
+    this.socket = new Websocket("ws://0.0.0.0:3001");
+    this.socket.onopen = event => {
+      console.log("Connected to server.");
+    }
+  }
+
+
+
 }
 export default App;
 
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,12 +3,29 @@
 import React, { Component } from 'react';
 
 class Chatbar extends Component {
+
+
+  handleKeyPressMessage(event) {
+    if (event.key === "Enter") {
+       this.props.onNewMessage(event);
+    }
+  }
+
+
+  handleKeyPressUser(event) {
+    if (event.key === "Enter") {
+       this.props.onUserChange(event);
+    }
+  }
+
   render() {
     console.log("Rendering <ChatBar/>");
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder= {this.props.currentUser.name} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" placeholder="Your Name"
+        defaultValue={this.props.currentUser.name} onKeyPress={this.handleKeyPressUser.bind(this)} />
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER"
+        onKeyPress={this.handleKeyPressMessage.bind(this)} />
       </footer>
     );
   }
