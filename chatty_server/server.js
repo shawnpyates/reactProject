e0ad504b64fd2d9a50472a965c56387bef66fcb2
Shawn Yates <yates.shawnp@gmail.com>
2017-05-05 23:53:23 +0000

    linted server file

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -15,9 +15,10 @@ function getRandomColor(arr) {
 }
 // Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
-  .use(express.static('public'))
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+// Make the express server serve static assets (html, javascript, css) from the /public folder
+.use(express.static('public'))
+.listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
 const wss = new SocketServer({ server });
@@ -37,39 +38,29 @@ wss.broadcast = function broadcast(data){
 // the ws parameter in the callback.
 
 wss.on('connection', (ws) => {
-  ws.send(JSON.stringify({type: "setColor", color: getRandomColor(colors)}));
+  ws.send(JSON.stringify({type: 'setColor', color: getRandomColor(colors)}));
   wss.broadcast({
-    type: "connectionCountChange",
+    type: 'connectionCountChange',
     content: wss.clients.size
   });
-  console.log('Client connected');
   ws.on('message', (message) => {
     const parsedMessage = JSON.parse(message);
     // logic here
-    if (parsedMessage.type === "postMessage") {
+    if (parsedMessage.type === 'postMessage') {
       parsedMessage.id = uuidV4();
-      parsedMessage.type = "incomingMessage";
-      let id = parsedMessage.id;
-      let username = parsedMessage.username;
-      let content = parsedMessage.content;
-      let color = parsedMessage.color;
-      console.log(`User ${username} with color of ${color} sent a message with the id ${id}. It said "${content}."`);
-    } else if (parsedMessage.type === "postNotification") {
+      parsedMessage.type = 'incomingMessage';
+    } else if (parsedMessage.type === 'postNotification') {
       parsedMessage.id = uuidV4();
-      parsedMessage.type = "incomingNotification";
-      let id = parsedMessage.id;
-      let username = parsedMessage.username;
-      console.log(`Notification with id ${id} sent for the user now known as ${username}.`);
+      parsedMessage.type = 'incomingNotification';
     }
     wss.broadcast(parsedMessage);
   });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => {
     wss.broadcast({
-      type: "connectionCountChange",
+      type: 'connectionCountChange',
       content: wss.clients.size
     });
-    console.log("Connection closed.");
   });
 });
 
